daa381da8eb0ce14ed6fd13f267d280aff0f9d8f
Michal Kubenka <mkubenka@gmail.com>
2020-05-07 17:21:17 +0200

    Add CentOS 8.

--- test/integration/default/default.rb
+++ test/integration/default/default.rb
@@ -1,9 +1,19 @@
-describe yum.repo('remi') do
-  it { should exist }
-  it { should be_enabled }
-end
-
 describe yum.repo('remi-safe') do
   it { should exist }
   it { should be_enabled }
 end
+
+if os.family == 'redhat' && os.release.to_f < 8
+  describe yum.repo('remi') do
+    it { should exist }
+    it { should be_enabled }
+  end
+end
+
+if os.family == 'redhat' && os.release.to_i >= 8
+  describe yum.repo('remi-modular') do
+    it { should exist }
+    it { should be_enabled }
+  end
+end
+
